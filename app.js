8501227a13e586b7a0b498b8d7d190815090c4e8
Rence Salaveria <rencesalaveria.dev@gmail.com>
2023-07-15 18:26:30 +0800

    Add home route get

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get("/", (req, res) => res.send("Hello World!"));
+
 app.post("/", (req, res) => {
-  console.log(JSON.stringify(req.body, 0, 2))
+  console.log(JSON.stringify(req.body, 0, 2));
   res.status(200).send(req.body);
 });
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
